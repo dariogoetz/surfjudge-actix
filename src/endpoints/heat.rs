d48bdf1cbaa6cdf31b630fd782e16959212e21ce
Dario Götz <dario.goetz@googlemail.com>
2020-10-04 22:15:22 +0200

    [mod] add category to heat object

--- src/endpoints/heat.rs
+++ src/endpoints/heat.rs
@@ -3,14 +3,14 @@ use crate::models::heat::Heat;
 
 use actix_web::{error, web, Result};
 
-pub async fn get_all_heats(db: web::Data<Pool>) -> Result<web::Json<Vec<Heat>>> {
+pub async fn get_all(db: web::Data<Pool>) -> Result<web::Json<Vec<Heat>>> {
     let result = Heat::find_all(db.get_ref()).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
     Ok(web::Json(result))
 }
 
-pub async fn get_heat_by_id(web::Path(heat_id): web::Path<u32>, db: web::Data<Pool>) -> Result<web::Json<Option<Heat>>> {
+pub async fn get_by_id(web::Path(heat_id): web::Path<u32>, db: web::Data<Pool>) -> Result<web::Json<Option<Heat>>> {
     let result = Heat::find_by_id(db.get_ref(), heat_id).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
--- src/models/heat.rs
+++ src/models/heat.rs
@@ -1,8 +1,26 @@
 use crate::database::Pool;
+use crate::models::category::Category;
+
+use futures::future;
 
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
+
+// this struct will be used to represent database record
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct HeatCore {
+    pub id: i32,
+    pub category_id: i32,
+    pub name: String,
+    pub round: i32,
+    pub number_in_round: i32,
+    pub start_datetime: NaiveDateTime,
+    pub number_of_waves: i32,
+    pub duration: f64,
+    pub heat_type: HeatType,
+    pub additional_info: String,
+}
 
 // this struct will be used to represent database record
 #[derive(Debug, Serialize, Deserialize, FromRow)]
@@ -17,6 +35,25 @@ pub struct Heat {
     pub duration: f64,
     pub heat_type: HeatType,
     pub additional_info: String,
+    pub category: Option<Category>,
+}
+
+impl From<HeatCore> for Heat {
+    fn from(heat: HeatCore) -> Self {
+        Heat {
+            id: heat.id,
+            category_id: heat.category_id,
+            name: heat.name,
+            round: heat.round,
+            number_in_round: heat.number_in_round,
+            start_datetime: heat.start_datetime,
+            number_of_waves: heat.number_of_waves,
+            duration: heat.duration,
+            heat_type: heat.heat_type,
+            additional_info: heat.additional_info,
+            category: None,
+        }
+    }
 }
 
 #[derive(sqlx::Type, Debug, Serialize, Deserialize)]
@@ -27,20 +64,35 @@ pub enum HeatType {
     Call,
 }
 
+async fn expand_heat(db: &Pool, heat_core: HeatCore) -> anyhow::Result<Heat> {
+    let category = Category::find_by_id(&db, heat_core.category_id as u32).await?;
+    let mut heat = Heat::from(heat_core);
+    heat.category = category;
+    Ok(heat)
+}
+
 impl Heat {
     pub async fn find_all(db: &Pool) -> anyhow::Result<Vec<Heat>> {
-        let heats = sqlx::query_as::<_, Heat>(r#"SELECT * FROM heats ORDER BY id"#)
+        let heats_core = sqlx::query_as::<_, HeatCore>(r#"SELECT * FROM heats ORDER BY id"#)
             .fetch_all(db)
             .await?;
-        Ok(heats)
+
+        let mut heats = Vec::new();
+        for heat_core in heats_core {
+            heats.push(expand_heat(&db, heat_core));
+        }
+        Ok(future::try_join_all(heats).await?)
     }
 
     pub async fn find_by_id(db: &Pool, heat_id: u32) -> anyhow::Result<Option<Heat>> {
-        let heat = sqlx::query_as::<_, Heat>(r#"SELECT * FROM heats WHERE id = $1"#)
+        let heat_core = sqlx::query_as::<_, HeatCore>(r#"SELECT * FROM heats WHERE id = $1"#)
             .bind(heat_id)
             .fetch_optional(db)
             .await?;
+        let heat = match heat_core {
+            Some(heat_core) => Some(expand_heat(&db, heat_core).await?),
+            None => None
+        };
         Ok(heat)
     }
-
 }
