a6f43f8aa6812d1a08302fd95ed2d1f41a18e218
Dario Götz <dario.goetz@googlemail.com>
2020-10-04 15:35:24 +0200

    [mod] return internal server error on database error

--- src/endpoints/base.rs
+++ src/endpoints/base.rs
@@ -1,12 +1,11 @@
 use crate::database::Pool;
 use crate::models::heat::Heat;
 
-use actix_web::{web, error, Result};
-
+use actix_web::{error, web, Result};
 
 pub async fn test_endpoint(db: web::Data<Pool>) -> Result<web::Json<Vec<Heat>>> {
-    let result = Heat::find_all(db.get_ref())
-        .await
-        .map_err(|e| error::ErrorInternalServerError(e))?;
+    let result = Heat::find_all(db.get_ref()).await.map_err(|e| {
+        error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
+    })?;
     Ok(web::Json(result))
 }
