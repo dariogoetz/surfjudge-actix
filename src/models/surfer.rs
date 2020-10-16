57d61c6794bb4b81aae6a930fcf733d04666da44
Dario Götz <dario.goetz@googlemail.com>
2020-10-16 21:53:49 +0200

    [add] unfinished advancing surfers functionality

--- src/models/surfer.rs
+++ src/models/surfer.rs
@@ -29,4 +29,23 @@ impl Surfer {
             .await?;
         Ok(surfer)
     }
+
+    pub async fn find_surfers_advancing_to_heat(db: &Pool, heat_id: u32) -> anyhow::Result<Vec<Self>> {
+        let adv = sqlx::query(
+            r#"SELECT r.surfer_id, r.heat_id, adv.seed
+            FROM surfers s
+                INNER JOIN results r
+                ON s.id = r.surfer_id
+                    INNER JOIN heat_advancements adv
+                    ON adv.to_heat_id = r.heat_id
+            WHERE adv.to_heat_id = $1"#
+        )
+            .bind(heat_id)
+            .fetch_all(db)
+            .await?;
+
+        // TODO: iterate through rows, collect heats and surfers and generate corresponding result structure
+        // TODO: fix lycra color if heat is a call
+        Ok(Vec::new())
+    }
 }
