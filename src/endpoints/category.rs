66e75fd6f06fb91a23b542013c339dc7f7aabe3b
Dario Götz <dario.goetz@googlemail.com>
2020-11-07 13:51:47 +0100

    [mod] do not expand data by default

--- src/endpoints/category.rs
+++ src/endpoints/category.rs
@@ -4,21 +4,21 @@ use crate::models::category::Category;
 use actix_web::{error, web, Result};
 
 pub async fn get_all(db: web::Data<Pool>) -> Result<web::Json<Vec<Category>>> {
-    let result = Category::find_all(db.get_ref(), true).await.map_err(|e| {
+    let result = Category::find_all(db.get_ref(), false).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
     Ok(web::Json(result))
 }
 
 pub async fn get_by_id(web::Path(category_id): web::Path<u32>, db: web::Data<Pool>) -> Result<web::Json<Option<Category>>> {
-    let result = Category::find_by_id(db.get_ref(), category_id, true).await.map_err(|e| {
+    let result = Category::find_by_id(db.get_ref(), category_id, false).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
     Ok(web::Json(result))
 }
 
 pub async fn get_by_tournament_id(web::Path(tournament_id): web::Path<u32>, db: web::Data<Pool>) -> Result<web::Json<Vec<Category>>> {
-    let result = Category::find_by_tournament_id(db.get_ref(), tournament_id, true).await.map_err(|e| {
+    let result = Category::find_by_tournament_id(db.get_ref(), tournament_id, false).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
     Ok(web::Json(result))
--- src/endpoints/heat.rs
+++ src/endpoints/heat.rs
@@ -11,7 +11,10 @@ pub struct HeatQuery {
 
 pub async fn get_all(query_params: web::Query<HeatQuery>, db: web::Data<Pool>) -> Result<web::Json<Vec<Heat>>> {
     let result = match query_params.into_inner() {
-        HeatQuery {category_id: Some(x)} => Heat::find_by_category_id(db.get_ref(), x as u32, true).await,
+        HeatQuery {category_id: Some(x)} => {
+            println!("Query params for heats are deprecated!");
+            Heat::find_by_category_id(db.get_ref(), x as u32, false).await
+        },
         _ => Heat::find_all(db.get_ref(), true).await
     }
     .map_err(|e| {
@@ -21,21 +24,28 @@ pub async fn get_all(query_params: web::Query<HeatQuery>, db: web::Data<Pool>) -
 }
 
 pub async fn get_by_id(web::Path(heat_id): web::Path<u32>, db: web::Data<Pool>) -> Result<web::Json<Option<Heat>>> {
-    let result = Heat::find_by_id(db.get_ref(), heat_id, true).await.map_err(|e| {
+    let result = Heat::find_by_id(db.get_ref(), heat_id, false).await.map_err(|e| {
+        error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
+    })?;
+    Ok(web::Json(result))
+}
+
+pub async fn get_by_category_id(web::Path(category_id): web::Path<u32>, db: web::Data<Pool>) -> Result<web::Json<Vec<Heat>>> {
+    let result = Heat::find_by_category_id(db.get_ref(), category_id, false).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
     Ok(web::Json(result))
 }
 
 pub async fn get_active_heats_by_tournament_id(web::Path(tournament_id): web::Path<u32>, db: web::Data<Pool>) -> Result<web::Json<Vec<Heat>>> {
-    let result = Heat::find_active_heats_by_tournament_id(db.get_ref(), tournament_id, true).await.map_err(|e| {
+    let result = Heat::find_active_heats_by_tournament_id(db.get_ref(), tournament_id, false).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
     Ok(web::Json(result))
 }
 
 pub async fn get_active_heats(db: web::Data<Pool>) -> Result<web::Json<Vec<Heat>>> {
-    let result = Heat::find_active_heats(db.get_ref(), true).await.map_err(|e| {
+    let result = Heat::find_active_heats(db.get_ref(), false).await.map_err(|e| {
         error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
     })?;
     Ok(web::Json(result))
