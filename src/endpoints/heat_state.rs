46724dc356c8b884b2e0528ad643b96a4bc4c15d
Dario Götz <dario.goetz@googlemail.com>
2021-03-06 20:25:49 +0100

    [mod] return json for remaining heat time; none for heat without state

--- src/endpoints/heat_state.rs
+++ src/endpoints/heat_state.rs
@@ -30,23 +30,23 @@ pub async fn get_by_heat_id(
 pub async fn get_remaining_heat_time(
     web::Path(heat_id): web::Path<u32>,
     db: web::Data<Pool>,
-) -> Result<Option<String>> {
-    let result = HeatState::find_by_heat_id(db.get_ref(), heat_id)
-        .await
-        .map_err(|e| {
-            error::ErrorInternalServerError(format!("Error fetching data from database: {:?}", e))
-        })?;
+) -> Result<web::Json<Option<f64>>> {
+
+    let result = match HeatState::find_by_heat_id(db.get_ref(), heat_id).await {
+        Ok(result) => result,
+        Err(_) => return Ok(web::Json(None))
+    };
+
     let result = result.map(|heat_state| {
-        let remaining_time_s = match heat_state.state {
+        match heat_state.state {
             HeatStateType::Paused => heat_state.remaining_time_s.unwrap_or(0.0),
             HeatStateType::Active => ((heat_state.end_datetime - Utc::now().naive_utc())
                 .num_milliseconds() as f64
                 / 1000.0)
                 .max(0.0),
             _ => heat_state.duration_m as f64 * 60.0,
-        };
-        format!("{}", remaining_time_s)
+        }
     });
 
-    Ok(result)
+    Ok(web::Json(result))
 }
