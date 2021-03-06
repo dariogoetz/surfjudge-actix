64e3fbb75d94613928b17b1648df9b446209793b
Dario Götz <dario.goetz@googlemail.com>
2021-03-06 20:26:42 +0100

    [mod] formatting

--- src/endpoints/heat_state.rs
+++ src/endpoints/heat_state.rs
@@ -31,21 +31,18 @@ pub async fn get_remaining_heat_time(
     web::Path(heat_id): web::Path<u32>,
     db: web::Data<Pool>,
 ) -> Result<web::Json<Option<f64>>> {
-
     let result = match HeatState::find_by_heat_id(db.get_ref(), heat_id).await {
         Ok(result) => result,
-        Err(_) => return Ok(web::Json(None))
+        Err(_) => return Ok(web::Json(None)),
     };
 
-    let result = result.map(|heat_state| {
-        match heat_state.state {
-            HeatStateType::Paused => heat_state.remaining_time_s.unwrap_or(0.0),
-            HeatStateType::Active => ((heat_state.end_datetime - Utc::now().naive_utc())
-                .num_milliseconds() as f64
-                / 1000.0)
-                .max(0.0),
-            _ => heat_state.duration_m as f64 * 60.0,
+    let result = result.map(|heat_state| match heat_state.state {
+        HeatStateType::Paused => heat_state.remaining_time_s.unwrap_or(0.0),
+        HeatStateType::Active => {
+            ((heat_state.end_datetime - Utc::now().naive_utc()).num_milliseconds() as f64 / 1000.0)
+                .max(0.0)
         }
+        _ => heat_state.duration_m as f64 * 60.0,
     });
 
     Ok(web::Json(result))
