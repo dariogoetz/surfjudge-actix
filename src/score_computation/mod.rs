673a8a742a34fdea6f53501570cd6c0446ffa60e
Dario Götz <dario.goetz@googlemail.com>
2021-08-29 21:48:55 +0200

    [mod] some refactoring

--- src/score_computation/mod.rs
+++ src/score_computation/mod.rs
@@ -12,6 +12,8 @@ pub trait ResultComputation {
     fn compute_results(heat_id: i32, judges: &[User], scores: &[Score]) -> Vec<Result>;
 }
 
+const PRECISION: i32 = 5;
+const N_BEST_WAVES: usize = 2;
 
 fn round_prec(val: f64, prec: i32) -> f64 {
     let d = (10.0_f64).powi(prec);
@@ -30,7 +32,7 @@ fn compute_individual_score(
     // TODO: sort scores by score
     // TODO: remove best and worst score
     // TODO: take mean of remaining scores
-    let score = scores.iter().map(|s| s.score).sum::<f64>() / scores.len() as f64;
+    let score = round_prec(scores.iter().map(|s| s.score).sum::<f64>() / scores.len() as f64, PRECISION);
     info!(LOG, "Averaged score: {}", score);
     WaveScore {
         surfer_id,
@@ -43,10 +45,9 @@ pub struct Default {}
 
 impl ResultComputation for Default {
     fn compute_results(heat_id: i32, judges: &[User], scores: &[Score]) -> Vec<Result> {
-        let n_best_waves = 2;
 
         // divide scores by wave id and surfer
-        let grouped_scores =
+        let scores_grouped =
             scores
                 .iter()
                 .fold(HashMap::<(i32, i32), Vec<&Score>>::new(), |mut acc, s| {
@@ -57,38 +58,43 @@ impl ResultComputation for Default {
                 });
 
         // compute individual results per wave and surfer (use compute_individual_score)
-        let wave_scores: Vec<(i32, i32, WaveScore)> = grouped_scores
+        let wave_scores: Vec<(i32, i32, WaveScore)> = scores_grouped
             .iter()
-            .map(|((surfer_id, wave), wave_scores)| {
+            .map(|((surfer_id, wave), individual_scores)| {
                 (
                     *surfer_id,
                     *wave,
-                    compute_individual_score(*surfer_id, *wave, judges, wave_scores),
+                    compute_individual_score(*surfer_id, *wave, judges, individual_scores),
                 )
             })
             .collect();
 
         // collect wave scores by surfer
-        let mut surfer_scores = wave_scores.iter().fold(
+        let mut scores_by_surfer = wave_scores.iter().fold(
             HashMap::<i32, Vec<&WaveScore>>::new(),
             |mut acc, (surfer_id, _, wave_score)| {
                 acc.entry(*surfer_id).or_insert(Vec::new()).push(wave_score);
                 acc
             },
         );
+        // sort surfer scores by wave number
+        scores_by_surfer.iter_mut().for_each(|(_, scores)| scores.sort_by(|s1, s2| s1.wave.cmp(&s2.wave)));
+
 
         // determine best n waves by surfer
-        let mut total_scores: Vec<(i32, Vec<f64>)> = surfer_scores
-            .iter_mut()
+        let mut ranking_scores: Vec<(i32, Vec<f64>)> = scores_by_surfer
+            .iter()
             .map(|(&surfer_id, wave_scores)| {
+                // make a copy of the vec for sorting by score
+                let mut sorted_scores = wave_scores.clone();
                 // sort waves by score
-                wave_scores.sort_by(|s1, s2| s2.score.partial_cmp(&s1.score).unwrap());
+                sorted_scores.sort_by(|s1, s2| s2.score.partial_cmp(&s1.score).unwrap());
 
                 // only take best n waves
                 let total_score: f64 =
-                    wave_scores.iter().take(n_best_waves).map(|s| round_prec(s.score, 5)).sum();
+                    sorted_scores.iter().take(N_BEST_WAVES).map(|s| round_prec(s.score, PRECISION)).sum();
 
-                let other_scores: Vec<f64> = wave_scores.iter().skip(n_best_waves).map(|s| round_prec(s.score, 5)).collect();
+                let other_scores: Vec<f64> = wave_scores.iter().skip(N_BEST_WAVES).map(|s| round_prec(s.score, PRECISION)).collect();
                 let mut rank_scores = Vec::new();
                 rank_scores.push(total_score);
                 rank_scores.extend(other_scores);
@@ -98,14 +104,14 @@ impl ResultComputation for Default {
             .collect();
 
         // sort surfer scores lexicographically by total score and then all other scores
-        total_scores.sort_by(|(_, s1), (_, s2)| s2.partial_cmp(s1).unwrap());
+        ranking_scores.sort_by(|(_, s1), (_, s2)| s2.partial_cmp(s1).unwrap());
 
         // if two surfers have exactily the same scores, they should have the same placing
         let mut results = Vec::new();
         let mut place: i32 = 0;
         let mut prev_place = 0;
         let mut prev_rank_scores: Option<&Vec<f64>> = None;
-        for (idx, (surfer_id, rank_scores)) in total_scores.iter().enumerate() {
+        for (idx, (surfer_id, rank_scores)) in ranking_scores.iter().enumerate() {
             if let Some(prev) = prev_rank_scores {
                 if *prev == *rank_scores {
                     place = prev_place;
@@ -120,13 +126,14 @@ impl ResultComputation for Default {
             if rank_scores.len() > 0 {
                 total_score = rank_scores[0];
             }
+            let wave_scores: Vec<WaveScore> = scores_by_surfer.get(&surfer_id).unwrap().iter().map(|&s| s.clone()).collect();
 
             results.push(Result {
                 surfer_id: *surfer_id,
                 heat_id,
                 place,
                 total_score,
-                wave_scores: surfer_scores.get(&surfer_id).unwrap().iter().map(|&s| s.clone()).collect(),
+                wave_scores,
                 heat: None,
                 surfer: None,
             });
